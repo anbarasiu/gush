636bb015d229c828293053011f1a42cc240096ba
Anbarasi <anbarasi@gigibenefits.com>
2020-04-22 15:25:44 +0530

    web-app-capable

--- src/components/layout.js
+++ src/components/layout.js
@@ -40,6 +40,10 @@ const Layout = ({ children, location }) => {
                 content: 'art, writing, poetry, story, inspiration',
               },
               {
+                name: 'apple-mobile-web-app-capable',
+                content: 'yes',
+              },
+              {
                 name: 'apple-mobile-web-app-status-bar-style',
                 content: 'black-translucent',
               },
